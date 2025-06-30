ad19d00cb5576ebccd2b8e786a35f6a0dcca6199
Virgil Ambrose Bombita <virgil-a.bombita@GXCHLAP00385.local>
2025-06-30 16:04:00 +0800

    Final Submission, Lab 2: Membership Calculator

---
+++ Readme.md
@@ -0,0 +1,9 @@
+Name: Virgil Ambrose C. Bombita
+
+Project Title: Lab 2: Membership Calculator
+
+Screenshots:
+
+![img.png](img.png)
+
+![img_1.png](img_1.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,48 @@
 package org.example;
 import java.util.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter membership status (Regular, VIP, Premium): ");
+        String membership = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = scanner.nextInt();
+        double price = 0.0;
+
+        if (membership.equalsIgnoreCase("Regular")) {
+            if (age > 64){
+                price = 75;
+            } else if (age > 18){
+                price = 100;
+            } else {
+                price = 50;
+            }
+        } else if (membership.equalsIgnoreCase("VIP")) {
+            if (age > 64){
+                price = 112.5;
+            } else if (age > 18){
+                price = 150;
+            } else {
+                price = 75;
+            }
+        } else if (membership.equalsIgnoreCase("Premium")) {
+            if (age > 64){
+                price = 150;
+            } else if (age > 18){
+                price = 200;
+            } else {
+                price = 100;
+            }
+        } else {
+            System.out.print("Invalid membership status entered.");
+        }
+
+        System.out.print("Price: $".concat(String.format("%.2f",price)));
     }
 }
